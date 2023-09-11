eda6c47ce81d7c4817ed8aa5f6ea136b2e2e60ca
phyothukha <phyothukha2193@gmail.com>
2023-09-11 12:09:01 +0630

    bs-scss

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// const text=document.createElement("h1");
+// const textnode=document.createTextNode("welcome");
+// const textvlaue=text.appendChild(textnode);
+
+// document.body.append(textvlaue);
+
+
+const h1=document.createElement('h1');
+document.body.h1.append("hello");
+
+
+
+
+
+
